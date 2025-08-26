7e8bfa5911f820aa8ce036994959430f0edf6182
rodims-code <ridrodims@hotmail.com>
2025-08-26 13:53:40 +0100

    Token, refresh token and create user

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 # Create your views here.
+
+class UserCreateView(generics.CreateAPIView) :
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
