e5bae057fa26a0eb5282db9c5f13445bf501a268
rodims-code <ridrodims@hotmail.com>
2025-08-26 17:16:27 +0100

    api, protected in fron-end

---
+++ frontend/src/components/ProtecterdRoute.jsx
@@ -0,0 +1,50 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import api from "../api";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+
+function ProtectedRoute({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+  const refreshToken = async () => {
+    const refrershToken = localStorage.getItem(REFRESH_TOKEN);
+    try {
+      const res = await api.post("/auth/token/refresh/", {
+        refresh: refrershToken,
+      });
+      if (res.status === 200) {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsAuthenticated(false);
+    }
+  };
+
+  const auth = async () => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (!token) {
+      setIsAuthenticated(false);
+      return;
+    }
+    const decode = jwtDecode(token);
+    const tokenExpiration = decode.exp;
+    const now = Date.now() / 1000;
+
+    if (tokenExpiration < now) {
+      await refreshToken();
+    } else {
+      setIsAuthenticated(true);
+    }
+  };
+
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
+  }
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
+}
+
+export default ProtectedRoute;
